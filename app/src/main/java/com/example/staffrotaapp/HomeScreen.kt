5353c2bec9c9c65cae50f766acdf50c71227e7d4
P2605346 <P2605346@CLPX0027WIN064W>
2024-01-10 15:58:55 +0000

    New Activities

--- app/src/main/java/com/example/staffrotaapp/HomeScreen.kt
+++ app/src/main/java/com/example/staffrotaapp/HomeScreen.kt
@@ -4,11 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class HomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+
+        val TimetableButton: Button = findViewById(R.id.TimetableBut)
+        TimetableButton.setOnClickListener {
+            val intent = Intent(this, Timetable::class.java)
+            startActivity(intent)
+        }
     }
 
     fun ButtonlogOut (View: View){
--- app/src/main/java/com/example/staffrotaapp/Timetable.kt
+++ app/src/main/java/com/example/staffrotaapp/Timetable.kt
@@ -1,11 +1,19 @@
 package com.example.staffrotaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Timetable : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timetable)
+
+        val TimetableretButton: Button = findViewById(R.id.button4)
+        TimetableretButton.setOnClickListener {
+            val intent = Intent(this, HomeScreen::class.java)
+            startActivity(intent)
+        }
     }
 }
