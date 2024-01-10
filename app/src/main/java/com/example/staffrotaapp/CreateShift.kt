8cacb1ce03a105297343add6ac2c451e38669964
P2605346 <P2605346@CLPX0027WIN064W>
2024-01-10 15:50:54 +0000

    New Activities

--- app/src/main/java/com/example/staffrotaapp/CreateShift.kt
+++ app/src/main/java/com/example/staffrotaapp/CreateShift.kt
@@ -1,11 +1,19 @@
 package com.example.staffrotaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CreateShift : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_shift)
+
+        val createShiftHome: Button = findViewById(R.id.Createshifthomebut)
+        createShiftHome.setOnClickListener {
+            val intent = Intent(this, AdminHome::class.java)
+            startActivity(intent)
+        }
     }
 }
