44e30cc22c6ebd48bb74a869519a3a4759797499
Ziggy <p2605346@my365.dmu.ac.uk>
2024-02-22 13:48:51 +0000

    displays current user in Profile

--- app/src/main/java/com/example/staffrotaapp/Profile.kt
+++ app/src/main/java/com/example/staffrotaapp/Profile.kt
@@ -1,14 +1,22 @@
 package com.example.staffrotaapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class Profile : AppCompatActivity() {
+    private lateinit var emailView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        emailView = findViewById(R.id.viewUsername)
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val userEmail = currentUser?.email
+        emailView.text = userEmail
     }
 
     fun ButtonReturnHome (View: View){
