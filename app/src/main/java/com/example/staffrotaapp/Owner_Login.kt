bfb24de8614d8e5455259838b712eabd7293d5ae
Ziggy <p2605346@my365.dmu.ac.uk>
2024-03-11 14:30:58 +0000

    owner login created and button fix on the admin login

--- app/src/main/java/com/example/staffrotaapp/Owner_Login.kt
+++ app/src/main/java/com/example/staffrotaapp/Owner_Login.kt
@@ -13,10 +13,6 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-
-private lateinit var username: EditText
-private lateinit var password: EditText
-private lateinit var LoginButton: Button
 class Owner_Login : AppCompatActivity() {
 
     // Firebase Database instance
