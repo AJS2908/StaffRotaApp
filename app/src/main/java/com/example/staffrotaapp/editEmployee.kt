af9106e44eedbcae4c0f3d756693ff9a3bd524c2
Ziggy <p2605346@my365.dmu.ac.uk>
2024-04-09 16:18:25 +0100

    edit employee class

---
+++ app/src/main/java/com/example/staffrotaapp/editEmployee.kt
@@ -0,0 +1,11 @@
+package com.example.staffrotaapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class editEmployee : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_employee)
+    }
+}
